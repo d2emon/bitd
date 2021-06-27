f91cb42ec11d2768f258e25048847a535997b34b
Dmitry Kutsenko <d2emon@users.noreply.github.com>
2021-06-27 09:04:27 +0300

    Initial Vue.js project

---
+++ front/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <app-header />
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+  components: {
+    AppHeader: () => import('@/components/AppHeader.vue'),
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+blockquote {
+  border-left: .25em solid #dfe2e5;
+  color: #6a737d;
+  padding: 0 1em;
+}
+</style>
